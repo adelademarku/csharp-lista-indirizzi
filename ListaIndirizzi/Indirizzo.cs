11c9cab31980db437142ed7f910d7e11f1a39a24
adelademarku <adelademarku@email.com>
2022-12-12 15:32:51 +0100

    Aggiungere i file di progetto.

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    public class Indirizzo
+    {
+        //PROPRITA'
+
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private int zipcodes;
+
+        public Indirizzo(string name, string surname, string street, string city, string province, int zipcodes)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zipcodes = zipcodes;
+
+        }
+        //GETTERS
+
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetSurname()
+        {
+            return surname;
+        }
+        public string GetStreet()
+        {
+            return street;
+        }
+        public string GetCity()
+        {
+            return city;
+        }
+        public string GetProvince()
+        {
+            return province;
+        }
+        public int GetZipCodes()
+        {
+            return zipcodes;
+        }
+    }
+}
---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Oggi esercitazione sui file, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe,
+//in particolare nel live-coding di oggi.
+//In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe,
+//e salvare tutti gli indirizzi contenuti 
+//al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+using ListaIndirizzi;
+
+Console.WriteLine("Hello, World!");
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+
+
+try
+{
+    StreamReader file = File.OpenText("C:\\Users\\Generation\\OneDrive\\Desktop\\Nuova cartella (2)\\ListaIndirizzi\\ListaIndirizzi\\Nuova cartella\\addresses.csv");
+
+    while (!file.EndOfStream)
+    {
+        string riga = file.ReadLine();
+
+
+        string[] informazioniIndirizzo = riga.Split(", ");
+
+        string name = informazioniIndirizzo[0];
+        string surname = informazioniIndirizzo[1];
+        string street = informazioniIndirizzo[2];
+        string city = informazioniIndirizzo[3];
+        string province = informazioniIndirizzo[4];
+        int zipcodes = int.Parse(informazioniIndirizzo[5]);
+
+
+        Indirizzo indirizzo = new Indirizzo(name,surname,street,city,province,zipcodes);
+        listaIndirizzi.Add(indirizzo);
+        // Console.WriteLine(riga);
+    }
+
+    file.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+foreach (Indirizzo indirizzo in listaIndirizzi)
+{
+    Console.WriteLine(indirizzo);
+}
